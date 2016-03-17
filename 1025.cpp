6b6938a23544684c1ac773276fa0081831baf1c9
siriushr <siriuslemon@gmail.com>
2016-03-17 13:19:58 +0800

    rt

--- 1025.cpp
+++ 1025.cpp
@@ -2,75 +2,58 @@
 //  1025.cpp
 //  myPlayground
 //
-//  Created by 黄然 on 15/12/4.
-//  Copyright © 2015年 黄然. All rights reserved.
+//  Created by 黄然 on 16/3/13.
+//  Copyright © 2016年 黄然. All rights reserved.
 //
 
 #include <iostream>
-#include <string>
 #include <vector>
-
 using namespace std;
 
-struct Line
-{
-    string addr;
+struct Node {
+    int addr;
     int data;
-    string next;
-    
-  
+    int next;
 };
 
-int myFind(vector<Line> a, string addr)
-{
-    int i = 0;
-    while (true) {
-        if (a[i].addr == addr)
-            return i;
-        ++i;
-    }
-}
-
-int main()
-{
-    string address;
+int main() {
+    vector<Node> vin(100000), vsorted, vout;
     int n, k;
-    vector<Line> a, b, c;
-    cin >> address >> n >> k;
+    int head;
+    cin >> head >> n >> k;
     
-    for (int i = 0; i < n; ++i) {
-        Line tmp;
+    Node tmp;
+    for (int i = 0; i < n; i++) {
         cin >> tmp.addr >> tmp.data >> tmp.next;
-        a.push_back(tmp);
+        vin[tmp.addr] = tmp;
+    }//输入完毕
+    
+    int cnt = 0;
+    while (head != -1) {
+        vsorted.push_back(vin[head]);
+        head = vin[head].next;
+        cnt++;
+    }//排序完毕
+    
+    int right = k-1;
+    while (right<cnt) {
+        for (int i = right; i>right-k; i--)
+            vout.push_back(vsorted[i]);
+        right +=k;
+    }//翻转链表
+    right -=k;
+    for (int i = right+1; i < cnt; i++) {
+        vout.push_back(vsorted[i]);
+    }//把剩余未翻转的部分加入输出链表
+    
+    //输出
+    int i = 0;
+    printf("%05d %d ", vout[i].addr, vout[i].data);
+    for (i = 1; i < cnt; i++){
+        printf("%05d\n%05d %d ", vout[i].addr, vout[i].addr, vout[i].data);
     }
     
-    int index = 0, cnt = 0;
-    while (n - cnt >= k) {
-        for (int i = 0; i < k; ++i) {
-            index = myFind(a, address);
-            
-            Line tmp;
-            tmp.addr = a[index].addr; tmp.data = a[index].data; tmp.next = a[index].next;
-            b.push_back(tmp);
-            
-            address = a[index].next;
-            ++cnt;
-        }
-        
-        reverse(b.begin(), b.end());
-        copy(b.begin(), b.end(), c.end());
-    }
+    printf("-1\n");
     
-    for (int i = 0; i < n - cnt; ++i) {
-        index = myFind(a, address);
-        c.push_back(a[index]);
-        address = a[index].next;
-    }
-    
-    for(auto b = c.begin(); b != c.end(); ++b) {
-        (*b).next = (*(b+1)).addr;
-    }
-    for (auto i: c)
-        cout << i.addr << " " << i.data << " " << i.next << endl;
     return 0;
 }
